17825c278ae31db8ee687f84b73aa33e5f1fb103
Matt <matt.white.624@gmail.com>
2021-07-09 23:31:57 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,12 +59,16 @@ var fight = function(enemyName) {
         } else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
-
-
     }
 }
 
 for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
